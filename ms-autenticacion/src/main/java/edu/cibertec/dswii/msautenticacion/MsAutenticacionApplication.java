f161f50ced55a478adffc3dd8c5e782e5f760103
charles.cordova <c.cordova@globant.com>
2021-05-31 20:57:51 -0500

    first commit

---
+++ ms-autenticacion/src/main/java/edu/cibertec/dswii/msautenticacion/MsAutenticacionApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.dswii.msautenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAutenticacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAutenticacionApplication.class, args);
+	}
+
+}
---
+++ ms-autenticacion/src/test/java/edu/cibertec/dswii/msautenticacion/MsAutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cibertec.dswii.msautenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
